b8248839ca3dc444c8afabdc2dfc2c09c980523f
JadeStep <140177325+JadeStep@users.noreply.github.com>
2021-08-07 20:41:12 +0000

    remove version

--- ngm/main.py
+++ ngm/main.py
@@ -20,3 +20,34 @@ TODO: Extend to images and categorical variables.
 import copy
 import networkx as nx
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import KFold
+import sys
+import torch
+import torch.nn as nn
+
+# local imports
+import ngm.utils.neural_view as neural_view
+import ngm.utils.data_processing as dp
+
+
+######################################################################
+# Functions for NGM learning
+######################################################################
+
+def product_weights_MLP(model):
+    """
+    Reads the input model (MLP) and returns the normalized
+    product of the neural network weight matrices. 
+    """
+    for i, (n, p) in enumerate(model.MLP.named_parameters()):
+        if i==0:
+            if 'weight' in n:
+                W = torch.abs(p).t() # DxH
+                # Normalizing the weight using L2-norm
+                W = torch.nn.functional.normalize(W)
+        else: # i > 0
+            if 'weight' in n:
+                curr_W = torch.abs(p).t()
+                # Normalizing the current weight using L2-norm
+                curr_W = torch.nn.functional.normalize(curr_W)
