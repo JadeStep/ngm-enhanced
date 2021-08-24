70d46450f796f372b9da6ef31267b9010db5ede5
JadeStep <140177325+JadeStep@users.noreply.github.com>
2021-08-24 08:24:57 +0000

    Version bump to 1.8.0

--- ngm/main.py
+++ ngm/main.py
@@ -520,3 +520,25 @@ def analyse_feature(target_feature, model_NGM, G, Xi=[]):
 # Getting the marginal distributions
 def marginal_distributions(model_NGM, X):
     """Get the marginal distribution for all the features learned by NGM.
+
+    1. Uses the probability sum law to calculate the marginals
+       P(A) = \sum_{n} P(A|B)
+    2. Use the histogram binning over the input data X (frequentist way)
+
+    Args:
+        model_NGM (list): [
+            model (torch.nn.object): A MLP model for NGM's `neural' view,
+            scaler (sklearn object): Learned normalizer for the input data,
+            feature_means (pd.Series): [feature:mean val]
+        ]
+        X (pd.DataFrame): Provided input samples.
+
+    Returns:
+        hist: Histogram (or function)
+    """
+    hist = X.hist(bins=100, figsize=(15, 15))
+    return hist
+
+
+######################################################################
+# Functions to sample from the learned NGM
