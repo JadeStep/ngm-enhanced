b412f697f54ad1e184cf682c38260ec7c52d24f3
JadeStep <140177325+JadeStep@users.noreply.github.com>
2021-08-29 08:48:34 +0000

    Updated Rakefile

--- ngm/main_generic.py
+++ ngm/main_generic.py
@@ -3,3 +3,34 @@
 Neural graphical models for the conditional 
 independence graphs. The conditional independence
 graphs show the partial correlations between the 
+nodes (features). 
+
+Functions for NGMs:
+1. Learning
+2. Inference
+3. Sampling
+
+Note that this implementation is for 
+1. Undirected graphs.
+2. Input data should be real valued.
+
+TODO: Implementation for the directed graphs. 
+TODO: Extend to images and categorical variables.
+"""
+import copy
+import networkx as nx
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import KFold
+from sklearn import preprocessing
+import sys
+import torch
+import torch.nn as nn
+
+# local imports
+import ngm.utils.neural_view as neural_view
+import ngm.utils.data_processing as dp
+
+
+######################################################################
+# Functions for NGM learning
