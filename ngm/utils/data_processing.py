b8cfcf22490bf07429c3c27888107d434684a336
JadeStep <140177325+JadeStep@users.noreply.github.com>
2022-03-20 06:21:41 +0000

    Removed unnecessary test

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -50,4 +50,5 @@ def function_plots_for_target(plot_dict):
 
 
 def plot_function(x, fx, title=f'plot of (x, fx)'):
-    # setting the axes at the 
+    # setting the axes at the centre
+    fig = plt.figu
