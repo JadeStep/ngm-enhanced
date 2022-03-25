f1be02486c50420cbea26fbb33048a2d0b0b3916
JadeStep <140177325+JadeStep@users.noreply.github.com>
2022-03-25 13:48:31 +0000

    Add basic layout

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -59,4 +59,5 @@ def plot_function(x, fx, title=f'plot of (x, fx)'):
     ax.spines['top'].set_color('none')
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
-    # plot the 
+    # plot the function
+    plt.plot(x, fx,
