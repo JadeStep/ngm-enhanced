537fdd7569e6e54520afb6a1e50354624756ab38
JadeStep <140177325+JadeStep@users.noreply.github.com>
2022-03-25 07:07:20 +0000

    Added layout

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -58,4 +58,5 @@ def plot_function(x, fx, title=f'plot of (x, fx)'):
     ax.spines['right'].set_color('none')
     ax.spines['top'].set_color('none')
     ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_tick
+    ax.yaxis.set_ticks_position('left')
+    # plot the 
