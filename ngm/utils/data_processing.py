2d9ceed930b6cce1e7b995663dc09a2606f3809c
JadeStep <140177325+JadeStep@users.noreply.github.com>
2022-05-26 16:35:13 +0000

    updated args

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -107,4 +107,4 @@ def get_interactive_graph(G, title='', node_PREFIX='ObsVal'):
 
 
 def set_feature_values(features_dict, features_known):
-    """Updates the feature value
+    """Updates the feature values with the known cat
