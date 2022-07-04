bfb54ee2d13f90d7586a4cd6ea106ca6b1f95802
JadeStep <140177325+JadeStep@users.noreply.github.com>
2022-07-04 06:08:51 +0000

    Use ESM

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -126,4 +126,4 @@ def set_feature_values(features_dict, features_known):
 
 def series2df(series):
     "Convert a pd.Series to pd.Dataframe and set the index as header."
-  
+    # Convert the serie
