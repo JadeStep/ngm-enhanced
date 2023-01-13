be94e8cd2fff5b66e35e787230c813ddef337beb
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-01-13 20:31:02 +0000

    2.8.4

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -191,4 +191,5 @@ def analyse_condition_number(table, MESSAGE=''):
      
 
 def eig_val_condition_num(A):
-    """Calculates the eigenvalues and the con
+    """Calculates the eigenvalues and the condition 
+    number of the 
