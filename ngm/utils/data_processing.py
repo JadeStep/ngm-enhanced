289ef30f136a34dc50f0a50ed75e241b688f5f4a
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-02-07 13:28:19 +0000

    // norms

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -202,4 +202,4 @@ def eig_val_condition_num(A):
 
 
 # Processing the input data to be compatiable for the CI graph recovery models
-def process_data_for_CI_graph(tab
+def process_data_for_CI_graph(table, NORM='min_max', ms
