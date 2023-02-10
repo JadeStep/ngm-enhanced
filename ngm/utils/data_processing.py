69026447d0e6cc012342b4e7cd16439d72f888c9
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-02-10 20:14:14 +0000

    Linux 3.0.36

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -204,4 +204,4 @@ def eig_val_condition_num(A):
 # Processing the input data to be compatiable for the CI graph recovery models
 def process_data_for_CI_graph(table, NORM='min_max', msg='', drop_duplicate=True):
     """Processing the input data to be compatiable for the 
-    regression network mode
+    regression network model. Checks for the 
