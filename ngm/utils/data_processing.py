84c088a4435236375034895ecc9729ca4c1ca045
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-03-16 08:49:04 +0000

    New Features

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -239,4 +239,4 @@ def process_data_for_CI_graph(table, NORM='min_max', msg='', drop_duplicate=True
     single_value_columns = []
     for col in table.columns:
         if len(table[col].unique()) == 1:
-            si
+            single_value_columns.append(c
