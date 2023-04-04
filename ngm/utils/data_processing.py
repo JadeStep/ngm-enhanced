cf35bb6d81f9feba63fa171597f0340bab19d96a
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-04-04 08:53:25 +0000

    updating the changelog

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -264,4 +264,5 @@ def process_data_for_CI_graph(table, NORM='min_max', msg='', drop_duplicate=True
 def get_cat_names(ohe, dtype):
     # Collecting the number of categories in cat features
     # categorical features in the original df. 
-    categorical_features = [k for k, v in dty
+    categorical_features = [k for k, v in dtype.items() if v=='c']
+    cat
