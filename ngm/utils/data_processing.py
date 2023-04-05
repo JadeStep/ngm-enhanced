c393d190f287ff3f0d308bbe2544b42382aca841
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-04-05 09:22:59 +0000

    Added GPLv3 License

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -266,4 +266,5 @@ def get_cat_names(ohe, dtype):
     # categorical features in the original df. 
     categorical_features = [k for k, v in dtype.items() if v=='c']
     cat_names = {}
-    for name, cat in zip(categorical_features
+    for name, cat in zip(categorical_features, ohe.categories_):
+        c
