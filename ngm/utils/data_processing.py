7caa5c7fa866c34809321ac24e940edc39e46bea
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-04-18 13:45:43 +0000

    Code tidying.

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -286,4 +286,4 @@ def convert_to_onehot(df, prefix=None):
 
 # Graph processing tools
 
-def plot_graph_compare(G, pos=None, title='', 
+def plot_graph_compare(G, pos=None, title='', scale_wt=1, intens
