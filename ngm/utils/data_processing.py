3f2be875f8aee9ef7e785c4c893b615e6d275768
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-04-21 08:34:24 +0000

    Added Travis CI

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -287,4 +287,5 @@ def convert_to_onehot(df, prefix=None):
 # Graph processing tools
 
 def plot_graph_compare(G, pos=None, title='', scale_wt=1, intensity=1):
-    edge_colors = [G.edges[e]['color'] for e in 
+    edge_colors = [G.edges[e]['color'] for e in G.edges]
+    edge_width = 
