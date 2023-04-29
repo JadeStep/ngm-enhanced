9220be27b0852511db9466b7a608b2fa5b8da307
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-04-29 10:41:27 +0000

    Security

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -292,4 +292,4 @@ def plot_graph_compare(G, pos=None, title='', scale_wt=1, intensity=1):
     plt.title(title, fontsize=20)
     n_edges = len(G.edges)
     if pos is None:
-        pos = nx.spring_layout(G, scal
+        pos = nx.spring_layout(G, scale=0.2, k=1/np.sqrt(n_ed
