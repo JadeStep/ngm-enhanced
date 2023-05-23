a23128f8b3e3ebd7e5189c8b1666c293ad667f97
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-05-23 21:47:48 +0000

    Serve static files

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -306,4 +306,4 @@ def compare_graphs(G1, G2, t1='Title1', t2='Title2', graph_type={'G1':'undirecte
     2. Finding the unique edges in graphs (edges in G1 and not in G2 and vice-versa)
     """
     # Find the common nodes between the graphs
-    c
+    common_nodes = set(G1.nodes
