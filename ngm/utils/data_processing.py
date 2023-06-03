6b5a6a0c8ccdc0aee07c98bbcb41ec472f7fc465
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-06-03 22:56:25 +0000

    Fix build on Windows.

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -315,4 +315,4 @@ def compare_graphs(G1, G2, t1='Title1', t2='Title2', graph_type={'G1':'undirecte
     # NOTE: There is some logic problem with the networkx 2.8.6 implementation
     # G_int = nx.intersection(G1_int, G2_int)
 
-    # p
+    # print(f'Nx function: G_int {G_
