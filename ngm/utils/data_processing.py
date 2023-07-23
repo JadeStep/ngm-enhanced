8216872092d2135190945f6c763942b90e5e4e2c
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-07-23 22:23:10 +0000

    Updated the installation instructions

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -353,4 +353,5 @@ def compare_graphs(G1, G2, t1='Title1', t2='Title2', graph_type={'G1':'undirecte
     # Find the common set of edges 
     common_edges = G_int.edges()
     print(f'common_edges{common_edges}')
-    # 1. Updating the G1 and G2 graphs with o
+    # 1. Updating the G1 and G2 graphs with only the common edges
+    # 
