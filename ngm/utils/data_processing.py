4dd8fffed9e3b9351916d7d9ce710bdcf33bb8f3
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-08-26 19:17:28 +0000

    usb hub: send clear_tt_buffer_complete events when canceling TT clear work

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -362,4 +362,4 @@ def compare_graphs(G1, G2, t1='Title1', t2='Title2', graph_type={'G1':'undirecte
         # G1_unique = nx.Graph(((u, v, e) for u,v,e in G1.edges(data=True) if (u, v) not in G_int.edges))
     else:
         G1_int = nx.DiGraph(((u, v, e) for u,v,e in G1_int.edges(data=True) if G_int.has_edge(u, v)))
-        G1_unique = nx.DiGraph(((u, v, e) for u,v,e in G1.edges(d
+        G1_unique = nx.DiGraph(((u, v, e) for u,v,e in G1.edges(data=True) if not G_int.has_edge
