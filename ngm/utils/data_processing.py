e6533fd1c8a49390d399fa854b85195500fb9b4b
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-08-27 18:42:07 +0000

    README.md update.

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -362,4 +362,6 @@ def compare_graphs(G1, G2, t1='Title1', t2='Title2', graph_type={'G1':'undirecte
         # G1_unique = nx.Graph(((u, v, e) for u,v,e in G1.edges(data=True) if (u, v) not in G_int.edges))
     else:
         G1_int = nx.DiGraph(((u, v, e) for u,v,e in G1_int.edges(data=True) if G_int.has_edge(u, v)))
-        G1_unique = nx.DiGraph(((u, v, e) for u,v,e in G1.edges(data=True) if not G_int.has_edge
+        G1_unique = nx.DiGraph(((u, v, e) for u,v,e in G1.edges(data=True) if not G_int.has_edge(u, v)))
+
+    if graph_type['G2']=='undir
