aedcf6ce6af0ee3d39fcd7cd0ee66d3740353595
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-08-31 11:08:11 +0000

    Rename field

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -364,4 +364,5 @@ def compare_graphs(G1, G2, t1='Title1', t2='Title2', graph_type={'G1':'undirecte
         G1_int = nx.DiGraph(((u, v, e) for u,v,e in G1_int.edges(data=True) if G_int.has_edge(u, v)))
         G1_unique = nx.DiGraph(((u, v, e) for u,v,e in G1.edges(data=True) if not G_int.has_edge(u, v)))
 
-    if graph_type['G2']=='undir
+    if graph_type['G2']=='undirected':
+        G2_int = nx.Gra
