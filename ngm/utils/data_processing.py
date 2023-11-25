3e85249f6a7062e396bfa955e805204e9af6b96b
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-11-25 22:22:44 +0000

    chore: upgrade dev dependencies

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -373,4 +373,5 @@ def compare_graphs(G1, G2, t1='Title1', t2='Title2', graph_type={'G1':'undirecte
 
     # if pos is None:
     pos = nx.spring_layout(G_int, scale=40, k=3/np.sqrt(G_int.order()))
-    # pos = nx.nx_agraph.graphviz_layout(G_int, prog='neato') #'fdp', 'sfdp', 'nea
+    # pos = nx.nx_agraph.graphviz_layout(G_int, prog='neato') #'fdp', 'sfdp', 'neato'
+    # nx.draw(G1_int, pos=
