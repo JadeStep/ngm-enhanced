7297130879241370994d59420a0d1de65dbd09d5
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-12-11 23:11:00 +0000

    Update application.xml

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -380,4 +380,4 @@ def compare_graphs(G1, G2, t1='Title1', t2='Title2', graph_type={'G1':'undirecte
 
     def get_scaling_wt(G):
         edge_width_G = np.array([abs(G.edges[e]['weight']) for e in G.edges])
-        # Sca
+        # Scaling the intensity of the edge
