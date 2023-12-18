aae508e5b52babea7e87d34af8c87871beb8c237
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-12-18 06:15:16 +0000

    Updated the neptune-client feedstock.

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -381,4 +381,4 @@ def compare_graphs(G1, G2, t1='Title1', t2='Title2', graph_type={'G1':'undirecte
     def get_scaling_wt(G):
         edge_width_G = np.array([abs(G.edges[e]['weight']) for e in G.edges])
         # Scaling the intensity of the edge_weights for viewing purposes
-        scale_wt_G = np.max(np.abs(edge_width_G)) if 
+        scale_wt_G = np.max(np.abs(edge_width_G)) if len(edge_width_G) > 0 else 1
