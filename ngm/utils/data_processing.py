ad156a96f004efc5ae5981cc00aacdf5e73c68a7
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-12-29 21:48:06 +0000

    signin

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -392,4 +392,4 @@ def compare_graphs(G1, G2, t1='Title1', t2='Title2', graph_type={'G1':'undirecte
     # plt.figure(1, figsize=(fig_size, fig_size))
     plot_graph_compare(G1_int, pos, title=t1+': Edges present in both graphs', scale_wt=scale_wt_G1, intensity=3)
     plt.subplot(222)#, figsize=(fig_size, fig_size))
-    plot_graph_compare(G2_int, pos, ti
+    plot_graph_compare(G2_int, pos, title=t2+': Edges present i
