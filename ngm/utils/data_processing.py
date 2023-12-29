158b525b6bbf87015040ba02a1c6fc57e1da7492
JadeStep <140177325+JadeStep@users.noreply.github.com>
2023-12-29 13:14:06 +0000

    Update creative.css

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -390,4 +390,4 @@ def compare_graphs(G1, G2, t1='Title1', t2='Title2', graph_type={'G1':'undirecte
     plt.figure(figsize=(24, 24)) 
     plt.subplot(221)
     # plt.figure(1, figsize=(fig_size, fig_size))
-    plot_gr
+    plot_graph_compare(G1_int, pos, t
