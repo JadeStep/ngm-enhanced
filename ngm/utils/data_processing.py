911ec334a5da906d9ba9e9ae049bacbf92374125
JadeStep <140177325+JadeStep@users.noreply.github.com>
2024-02-20 16:04:23 +0000

    werwer

--- ngm/utils/data_processing.py
+++ ngm/utils/data_processing.py
@@ -405,4 +405,5 @@ def compare_graphs(G1, G2, t1='Title1', t2='Title2', graph_type={'G1':'undirecte
     plt.savefig(buf)
     # getting the image in bytes
     buf.seek(0)
-    image_bytes = buf.getvalue() # Image.
+    image_bytes = buf.getvalue() # Image.open(buf, mode='r')
+    bu
